c8d7bdc23b49e0c84ba5b1489f10cd4807668428
rbrtsmith <rbrtsmith84@googlemail.com>
2017-07-21 15:08:53 +0100

    remove uncecessary files

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-
-import App from './App'
-import './scss/main'
-
-const appTitle = 'Welcome to your React + Nebula project'
-
-render(<App title={appTitle} />, document.querySelector('#app'));
