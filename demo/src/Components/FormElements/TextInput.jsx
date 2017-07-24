4ff15647ceb404d8c57d2bcd40df75059b05f814
Kay Wood <kwood15@users.noreply.github.com>
2017-07-24 18:13:26 +0100

    Add search field

--- demo/src/Components/FormElements/TextInput.jsx
+++ demo/src/Components/FormElements/TextInput.jsx
@@ -16,6 +16,13 @@ const TextInput = () => (
         <label htmlFor="text-input-3" className="c-label">Message:</label>
         <textarea id="text-input-3" className="c-text-input c-text-input--textarea" placeholder="Your message…" />
       </div>
+      <div className="o-grid__item u-1/2@sm">
+        <label htmlFor="text-input-4" className="c-label">Search:</label>
+        <div className="c-text-search">
+          <input type="search" id="text-input-4" name="search" placeholder="Search..." className="c-text-input c-text-input--search" />
+          <button type="submit" className="c-btn c-btn--alpha c-btn--md c-btn-icon c-btn-icon--search" />
+        </div>
+      </div>
     </div>
   </div>
 )
