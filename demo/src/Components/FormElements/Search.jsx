7c128a5d56d95ffa8b0e963848a29d06198b8090
Kay Wood <kwood15@users.noreply.github.com>
2017-07-25 13:45:23 +0100

    Add small size modifiers for text/search inputs

--- demo/src/Components/FormElements/Search.jsx
+++ demo/src/Components/FormElements/Search.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
 
 const Search = () => (
-  <div className="o-grid">
-    <div className="o-grid__item u-1/2@sm">
-      <label htmlFor="search" className="c-label">Search:</label>
-      <div className="c-search c-search--submit-right">
-        <input type="text" id="search" placeholder="Search…" className="c-text-input c-text-input--sm c-search__input" />
-        <button className="c-search__submit c-btn c-btn--alpha" title="submit" />
+  <div>
+    <h4>Search: Medium</h4>
+    <div className="o-grid o-grid--gutter-md o-grid--matrix">
+      <div className="o-grid__item u-1/2">
+        <label htmlFor="search" className="c-label">Label</label>
+        <div className="c-search c-search--submit-left">
+          <input type="text" id="search" placeholder="Search…" className="c-text-input c-search__input" />
+          <button className="c-search__submit c-btn c-btn--alpha" title="submit" />
+        </div>
+      </div>
+      <div className="o-grid__item u-1/2">
+        <label htmlFor="search" className="c-label">Label</label>
+        <div className="c-search c-search--submit-right">
+          <input type="text" id="search" placeholder="Search…" className="c-text-input c-search__input" />
+          <button className="c-search__submit c-btn c-btn--alpha" title="submit" />
+        </div>
       </div>
     </div>
+    <hr />
+    <h4>Search: Small</h4>
+    <div className="o-grid o-grid--gutter-md o-grid--matrix">
+      <div className="o-grid__item">
+        <label htmlFor="search" className="c-label">Label</label>
+        <div className="c-search c-search--submit-left">
+          <input type="text" id="search" placeholder="Search…" className="c-text-input c-text-input--sm c-search__input" />
+          <button className="c-search__submit c-btn c-btn--alpha" title="submit" />
+        </div>
+      </div>
+    </div>
+    <hr />
   </div>
 )
 
--- demo/src/Components/FormElements/TextInput.jsx
+++ demo/src/Components/FormElements/TextInput.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TextInput = () => (
   <div>
-    <h4>Text input</h4>
+    <h4>Text input: Medium</h4>
     <div className="o-grid o-grid--gutter-md o-grid--matrix">
       <div className="o-grid__item u-1/2@sm">
         <label htmlFor="text-input-1" className="c-label">Forename:</label>
@@ -17,6 +17,15 @@ const TextInput = () => (
         <textarea id="text-input-3" className="c-text-input c-text-input--textarea" placeholder="Your message…" />
       </div>
     </div>
+    <hr />
+    <h4>Text input: Small</h4>
+    <div className="o-grid o-grid--gutter-md o-grid--matrix">
+      <div className="o-grid__item u-1/2@sm">
+        <label htmlFor="text-input-1" className="c-label">Forename:</label>
+        <input type="text" id="text-input-1" placeholder="Nebula" className="c-text-input c-text-input--sm" />
+      </div>
+    </div>
+    <hr />
   </div>
 )
 
