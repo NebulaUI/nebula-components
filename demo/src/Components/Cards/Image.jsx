efe11d1f2aa55e8068c1f6f811fe66590c087250
rbrtsmith <rbrtsmith84@googlemail.com>
2017-07-27 15:24:50 +0100

    Remove redundant files

--- demo/src/Components/Cards/Image.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Image = () => (
-  <div>
-    <div className="c-card__img c-card__img--placeholder c-card__img--top" />
-  </div>
-)
-
-export default Image
--- demo/src/Components/Cards/Title.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Title = () => (
-  <h4 className="c-card-block__title">Card title</h4>
-)
-
-export default Title
