12b2a737cd699334c67fcc51ffd46ca0d38ddd3f
Kay Wood <kwood15@users.noreply.github.com>
2017-08-04 13:54:31 +0100

    Fix linting

--- demo/src/Components/Modal/index.jsx
+++ demo/src/Components/Modal/index.jsx
@@ -8,7 +8,7 @@ const Modal = ({ isOpen, close, children }) => (
     <button className="c-modal__overlay" onClick={close} />
     <div className="c-modal__body">
       <div className="c-modal__content">
-        <button title="Close" className="c-modal__dismiss c-btn--sm" onClick={close}></button>
+        <button title="Close" className="c-modal__dismiss c-btn--sm" onClick={close}>Close me pretty please...</button>
         {children}
       </div>
     </div>
