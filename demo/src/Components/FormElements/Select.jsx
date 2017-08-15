c885fe3b8fabed54020c6753647839561cccc11d
Kay Wood <kwood15@users.noreply.github.com>
2017-08-15 11:51:24 +0100

    Add Select component - small

--- demo/src/Components/FormElements/Select.jsx
+++ demo/src/Components/FormElements/Select.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 const Select = () => (
   <div>
-    <h4>Select</h4>
+    <h4>Select: Medium</h4>
     <select className="c-select">
       <option defaultValue>Select option</option>
       <option value="">Value 1</option>
     </select>
+    <h4>Select: Small</h4>
+    <div className="o-grid__item u-1/2@sm">
+      <select className="c-select c-select--sm">
+        <option defaultValue>Select option</option>
+        <option value="">Value 1</option>
+      </select>
+    </div>
   </div>
 )
 
