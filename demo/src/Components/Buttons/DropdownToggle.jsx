85dab01d077966063a9fed0fcbc004bfbe2ac201
Robert Smith <rbrtsmith84@googlemail.com>
2017-08-30 11:12:27 +0100

    Fix linting issues in button dropdown

--- demo/src/Components/Buttons/DropdownToggle.jsx
+++ demo/src/Components/Buttons/DropdownToggle.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import T from 'prop-types'
 
-const DropdownToggle = ({ handleToggle, className }) => (
+const DropdownToggle = ({ handleToggle }) => (
   <button className="c-btn-dropdown__toggle" onClick={handleToggle} />
 )
 
 
 DropdownToggle.propTypes = {
   handleToggle: T.func,
-  className: T.string
 }
 
 export default DropdownToggle
