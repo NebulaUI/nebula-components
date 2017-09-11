dde1808d700174814e22243a11066a2c9014d898
Kay Wood <kwood15@users.noreply.github.com>
2017-09-11 15:53:06 +0100

    Refactor

--- demo/src/Components/Pills/index.jsx
+++ demo/src/Components/Pills/index.jsx
@@ -4,34 +4,34 @@ const Pills = () => (
   <div>
     <section className="o-section-md">
       <div className="o-site-wrap o-site-wrap--padding">
-        <h4>Pills: Large</h4>
+        <h4>Pills</h4>
         <ul className="o-matrix-list o-matrix-list--spaced-md">
           <li className="o-matrix-list__item">
-            <button className="c-pill c-pill--xs c-pill--border">Base</button>
+            <button className="c-pill c-pill--border">Base</button>
           </li>
           <li className="o-matrix-list__item">
-            <button className="c-pill c-pill--xs c-pill--alpha">Alpha</button>
+            <button className="c-pill c-pill--alpha">Alpha</button>
           </li>
           <li className="o-matrix-list__item">
-            <button className="c-pill c-pill--xs c-pill--beta">Beta</button>
+            <button className="c-pill c-pill--beta">Beta</button>
           </li>
           <li className="o-matrix-list__item">
-            <button className="c-pill c-pill--xs is-active">Active</button>
+            <button className="c-pill is-active">Active</button>
           </li>
           <li className="o-matrix-list__item">
-            <button className="c-pill c-pill--xs" disabled>Disabled</button>
+            <button className="c-pill" disabled>Disabled</button>
           </li>
           <li className="o-matrix-list__item">
-            <button className="c-pill c-pill--xs c-pill--success">Success</button>
+            <button className="c-pill c-pill--success">Success</button>
           </li>
           <li className="o-matrix-list__item">
-            <button className="c-pill c-pill--xs c-pill--info">Info</button>
+            <button className="c-pill c-pill--info">Info</button>
           </li>
           <li className="o-matrix-list__item">
-            <button className="c-pill c-pill--xs c-pill--warning">Warning</button>
+            <button className="c-pill c-pill--warning">Warning</button>
           </li>
           <li className="o-matrix-list__item">
-            <button className="c-pill c-pill--xs c-pill--error">Error</button>
+            <button className="c-pill c-pill--error">Error</button>
           </li>
         </ul>
       </div>
