66ac0b2b0924b4c6467adf98160148bdd242daf6
Kay Wood <kwood15@users.noreply.github.com>
2017-09-25 17:39:22 +0100

    Allow for insertion of Icons on status cards #40

--- demo/src/Components/Cards/StatusCards/index.jsx
+++ demo/src/Components/Cards/StatusCards/index.jsx
@@ -7,35 +7,42 @@ const StatusCards = () => (
     <div className="o-site-wrap o-site-wrap--padding">
       <h4>Status Cards</h4>
       <div className="o-grid o-grid--gutter-md o-grid--matrix">
-
         <div className="o-grid__item u-1/2@sm">
-          <div className="c-status-card c-status-card--success">
-            <h3>Success</h3>
-            <Text />
+          <div className="c-status-card">
+            <div className="o-flag__component c-status-card__component c-status-card--success" />
+            <div className="o-flag__body c-status-card__body">
+              <h3>Success</h3>
+              <Text />
+            </div>
           </div>
         </div>
-
         <div className="o-grid__item u-1/2@sm">
-          <div className="c-status-card c-status-card--info">
-            <h3>Info</h3>
-            <Text />
+          <div className="c-status-card">
+            <div className="o-flag__component c-status-card__component c-status-card--info" />
+            <div className="o-flag__body c-status-card__body">
+              <h3>Info</h3>
+              <Text />
+            </div>
           </div>
         </div>
-
         <div className="o-grid__item u-1/2@sm">
-          <div className="c-status-card c-status-card--warning">
-            <h3>Warning</h3>
-            <Text />
+          <div className="c-status-card">
+            <div className="o-flag__component c-status-card__component c-status-card--warning" />
+            <div className="o-flag__body c-status-card__body">
+              <h3>Warning</h3>
+              <Text />
+            </div>
           </div>
         </div>
-
         <div className="o-grid__item u-1/2@sm">
-          <div className="c-status-card c-status-card--error">
-            <h3>Error</h3>
-            <Text />
+          <div className="c-status-card">
+            <div className="o-flag__component c-status-card__component c-status-card--error" />
+            <div className="o-flag__body c-status-card__body">
+              <h3>Error</h3>
+              <Text />
+            </div>
           </div>
         </div>
-
       </div>
     </div>
   </section>
